b238ec173e20f75722783c28ad84fa1a7cc30f60
falogre <floppy-88@mail.ru>
2013-08-10 23:35:29 +0400

    Create models1

--- Products/models.py
+++ Products/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class Products(models.Model):
     name = models.CharField(max_length=100, verbose_name='Название')
+    price = models.DecimalField(decimal_places=2, max_digits=99999, verbose_name='Цена')
